0097ff81163bc554898863f9f89a289f8e10dd78
tpphu <vietean@gmail.com>
2016-03-02 19:33:11 +0700

    Fix bug pass opts

--- src/fetchUrl.js
+++ src/fetchUrl.js
@@ -4,13 +4,13 @@ var utils = require("../utils");
 var log = require("npmlog");
 
 module.exports = function(defaultFuncs, api, ctx) {
-  return function fetchUrl(url, callback) {
+  return function fetchUrl(url, opts, callback) {
     if(!callback) {
       throw {error: "fetchUrl: need callback"};
     }
 
     defaultFuncs
-      .get(url, ctx.jar)    
+      .get(url, ctx.jar, opts)
       .then(utils.saveCookies(ctx.jar))
       .then(function(res) {
         callback(null, res.body);
